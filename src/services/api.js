30efe082cd936e0d5c4511e5229fd652d79920cb
Shashank-1K <96000384+Shashank-1K@users.noreply.github.com>
2025-05-28 17:28:33 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,11 @@
-const API_BASE = 'https://api.jsonbin.io/v3/b';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001';
 
 export const fetchFleetSummary = async () => {
   try {
-    const res = await fetch(`${API_BASE}/6836f5128561e97a501c8a33`);
-    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
-    const data = await res.json();
-    console.log("working Fetching");
-    console.log(data);
-    return data.record;  // actual JSON content inside "record"
+    const response = await axios.get(`${API_URL}/fleetSummary`);
+    return response.data;
   } catch (error) {
     console.error('Error fetching fleet summary dff:', error);
     throw error;
@@ -15,13 +13,11 @@ export const fetchFleetSummary = async () => {
 };
 
 export const fetchUsageData = async (range) => {
-  const endpoint = range === '7' ? '6836f46d8a456b7966a6674d' : 
-                   range === '15' ? '6836f4da8a456b7966a66791' : '6836f4f98a456b7966a6679c';
+  const endpoint = range === '7' ? 'weeklyUsage' : 
+                  range === '15' ? 'biweeklyUsage' : 'monthlyUsage';
   try {
-    const res = await fetch(`${API_BASE}/${endpoint}`);
-    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
-    const data = await res.json();
-    return data.record;
+    const response = await axios.get(`${API_URL}/${endpoint}`);
+    return response.data;
   } catch (error) {
     console.error(`Error fetching ${range} day usage data:`, error);
     throw error;
